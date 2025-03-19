c6028348e3611edcb8fefb455f55dfe01e1dac78
Sudipta Halder <66485000+SoupBoi1@users.noreply.github.com>
2025-03-19 09:34:05 -0500

    header file

---
+++ ST7789VW.h
@@ -0,0 +1,24 @@
+#ifndef HEADER_ST7789VW
+#define HEADER_ST7789VW
+
+#define RDDSDR 0x0F // Read display
+#define SLPIN 0x10 //Sleep in 
+#define SLPOUT 0x11 //Sleep out
+#define PTLON 0x12 //Partial mode on
+#define NORON 0x13 //Partial off 
+#define INVOFF 0x20 //Display inversion off
+#define INVON 0x21  //Display inversion on
+#define GAMSET 0x26 // Display inversion  on 
+#define DISPOFF 0x28 // Display off 
+#define DISPON 0x29 // Display off 
+#define CASET 0x2A //  Column address set
+#define RASET 0x2B //  Row address set
+
+
+
+
+
+
+#endif
+
+
